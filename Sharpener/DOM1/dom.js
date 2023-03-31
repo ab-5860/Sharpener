29276fa7334eeef7bdadb8e9112791b9e825fb4e
ab-5860 <kmrabhi9987@gmail.com>
2023-03-31 12:26:55 +0530

    Get Elements By Tag Name

--- Sharpener/DOM1/dom.js
+++ Sharpener/DOM1/dom.js
@@ -32,22 +32,37 @@
 
 // GET ELEMENTS BY CLASS NAME
 
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
 
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.background = 'yellow';
-// items.style.background = '#f4f4f4';
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.background = 'yellow';
+// // items.style.background = '#f4f4f4';
 
-for(var i =0; i<items.length;i++)
+// for(var i =0; i<items.length;i++)
+// {
+//     items[i].style.background = '#f4f4f4';
+// }
+
+// items[2].style.background = 'green';
+// for(var i = 0;i<items.length;i++)
+// {
+//     items[i].style.fontWeight = 'bold';
+// }
+
+// GET ELEMENT BY TAG NAME
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+
+li[1].textContent =   'Hello 2';
+li[1].style.fontWeight = 'bold';
+li[1].style.background = 'yellow';
+
+for(var i=0; i <li.length;i++)
 {
-    items[i].style.background = '#f4f4f4';
-}
-
-items[2].style.background = 'green';
-for(var i = 0;i<items.length;i++)
-{
-    items[i].style.fontWeight = 'bold';
+    li[i].style.background = '#f4f4f4';
 }
